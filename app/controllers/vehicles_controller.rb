5019260def072624cdb8c9b436a48fb3a125be1b
Hernan Galvez <hgalvezNeculman@gmail.com>
2019-05-20 14:41:49 -0400

    Controller & View Vehicle: Index, Show, Create

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,2 +1,27 @@
 class VehiclesController < ApplicationController
+
+    def index
+        @vehicles = Vehicle.all
+    end
+
+    def new
+    end
+
+    def create
+        #render plain: params[:vehicle].inspect
+        @vehicle = Vehicle.new vehicle_params
+        @vehicle.save
+
+        redirect_to @vehicle
+    end
+
+    def show
+        @vehicle = Vehicle.find params[:id]
+    end
+
+    private 
+
+    def vehicle_params
+        params.require(:vehicle).permit(:vehicle_identifier)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :vehicles
+
+  #get '/vehicles/new', to: 'vehicles#new'
+  #post '/vehicles', to: 'vehicles#create'
+  #get  '/vehicles/:vehicle_identifier', to: 'vehicles#show'
 end
